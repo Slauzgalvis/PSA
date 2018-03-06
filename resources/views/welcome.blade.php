fbdad8de629d7d31ac0588183e6609caf2711b4e
Slauzgalvis <slauzgalvis.d@gmail.com>
2018-03-06 23:01:57 +0200

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WorkIT
                 </div>
 
                 <div class="links">
